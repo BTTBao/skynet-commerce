7a0505def2024e87697e26cd9f8a7a84bd608408
Lê Nguyễn Anh Dự <dule1028a@gmail.com>
2025-11-19 23:58:44 +0700

    cập nhật entities

---
+++ Skynet-Commerce/Skynet-Commerce/DAL/Entities/Order.cs
@@ -0,0 +1,55 @@
+namespace Skynet_Commerce.DAL.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Order
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+            OrderShippingInfoes = new HashSet<OrderShippingInfo>();
+            OrderStatusHistories = new HashSet<OrderStatusHistory>();
+        }
+
+        public int OrderID { get; set; }
+
+        public int OrderGroupID { get; set; }
+
+        public int ShopID { get; set; }
+
+        public int AccountID { get; set; }
+
+        public int? AddressID { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public bool? IsReviewed { get; set; }
+
+        public virtual Account Account { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+        public virtual OrderGroup OrderGroup { get; set; }
+
+        public virtual UserAddress UserAddress { get; set; }
+
+        public virtual Shop Shop { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderShippingInfo> OrderShippingInfoes { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderStatusHistory> OrderStatusHistories { get; set; }
+    }
+}
