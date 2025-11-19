967a1d6125193fcb2306ec6a2e4ab1f2d2844788
Lê Nguyễn Anh Dự <dule1028a@gmail.com>
2025-11-19 11:07:06 +0700

    Thêm giao diện admin: categories

---
+++ Skynet-Commerce/Skynet-Commerce/GUI/UserControls/Components/Admin/UcCategoryRow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Skynet_Commerce.GUI.UserControls
+{
+    public partial class UcCategoryRow : UserControl
+    {
+        public UcCategoryRow()
+        {
+            InitializeComponent();
+            // Kẻ đường gạch chân mờ
+            this.Paint += (s, e) => {
+                e.Graphics.DrawLine(new Pen(Color.FromArgb(240, 240, 240)), 0, this.Height - 1, this.Width, this.Height - 1);
+            };
+        }
+
+        public void SetData(string id, string name, string totalProducts, string subcategories)
+        {
+            _lblId.Text = id;
+            _lblName.Text = name;
+            _lblProducts.Text = totalProducts;
+            _lblSubcats.Text = subcategories;
+        }
+    }
+}
