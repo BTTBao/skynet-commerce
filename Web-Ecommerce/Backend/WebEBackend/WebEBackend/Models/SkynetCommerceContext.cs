adbe92eedb148c9be7447efce4f98385f73a2158
BTTBao <lebao.071205@gmail.com>
2026-01-28 23:00:47 +0700

    fix nhẹ

--- Web-Ecommerce/Backend/WebEBackend/WebEBackend/Models/SkynetCommerceContext.cs
+++ Web-Ecommerce/Backend/WebEBackend/WebEBackend/Models/SkynetCommerceContext.cs
@@ -72,7 +72,7 @@ public partial class SkynetCommerceContext : DbContext
     public virtual DbSet<Wishlist> Wishlists { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 
     => optionsBuilder.UseSqlServer("Data Source=DESKTOP-D40AP7A\\SQLEXPRESS;Initial Catalog=Skynet_commerce;Integrated Security=True;Trust Server Certificate=True");
     //  => optionsBuilder.UseSqlServer("Data Source=DESKTOP-9E807K6;Initial Catalog=Skynet_commerce;Integrated Security=True;Trust Server Certificate=True");
